d93f126c6d22d2e2c6ce5907dfb9418aafab78eb
Jonathan García <jona03g97@gmail.com>
2021-11-01 16:07:27 -0600

    Start webpack config

---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+const path = require('path')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const webackDevServer = require('webpack-dev-server')
