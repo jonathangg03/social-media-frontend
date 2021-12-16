5da83d0fd1422fa418865eb555e22fe7746fd803
Jonathan García <jona03g97@gmail.com>
2021-12-16 00:18:07 -0600

    Start new version

--- src/components/User/index.js
+++
@@ -1,9 +0,0 @@
-export default function User({ name, description, profilePicture }) {
-  return (
-    <li>
-      <img src={profilePicture} alt={name} />
-      <h3>{name}</h3>
-      <h4>{description}</h4>
-    </li>
-  )
-}
