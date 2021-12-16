aa1c5bd38552fa3a3cb31e7f8c54ab2f74360ecd
Jonathan García <jona03g97@gmail.com>
2021-12-16 01:30:14 -0600

    Create Liked page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from './pages/Main'
 import Home from './pages/Home'
+import Liked from './pages/Liked'
 // import Profile from './pages/Profile'
 // import NewIdea from './pages/NewIdea'
 // import NotFound from './pages/NotFound'
-// import Liked from './pages/Liked'
 // import EditProfile from './pages/EditProfile'
 // import Search from './pages/Search'
 
@@ -14,9 +14,9 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/liked' element={<Liked />} />
         {/* <Route path='/:id/profile' element={<Profile />} />
         <Route path='/:id/profile/edit' element={<EditProfile />} />
-        <Route path='/liked' element={<Liked />} />
         <Route path='/search' element={<Search />} />
         <Route path='/search/:id/' element={<Profile />} />
         <Route path='/:id/newIdea' element={<NewIdea />} />
