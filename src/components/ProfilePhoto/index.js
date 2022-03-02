c46bca081196d3ae25952e7ee190d744d0b1afa9
Jonathan García <megabyte9703@gmail.com>
2022-03-02 08:30:54 -0600

    Edit: ProfilePhoto

--- src/components/ProfilePhoto/index.js
+++ src/components/ProfilePhoto/index.js
@@ -1,15 +1,13 @@
 import defaultProfilePhoto from '../../../public/defaultProfilePhoto.jpg'
 import './index.scss'
 
-export default function ProfilePhoto({ profile }) {
+export default function ProfilePhoto({ profilePhotoUrl, name }) {
   return (
     <figure className='profilePhoto'>
-      {profile.profilePhotoUrl === undefined && <img src={null} />}
-      {profile.profilePhotoUrl === '' && (
-        <img src={defaultProfilePhoto} alt={profile.name} />
-      )}
-      {profile.profilePhotoUrl && profile.profilePhotoUrl.length > 0 && (
-        <img src={profile.profilePhotoUrl} alt={profile.name} />
+      {profilePhotoUrl === undefined && <img src={null} />}
+      {profilePhotoUrl === '' && <img src={defaultProfilePhoto} alt={name} />}
+      {profilePhotoUrl && profilePhotoUrl.length > 0 && (
+        <img src={profilePhotoUrl} alt={name} />
       )}
     </figure>
   )
