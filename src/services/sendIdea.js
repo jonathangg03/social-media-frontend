2d42de871ee115d4ea23a67145ff59020a667ba4
Jonathan García <megabyte9703@gmail.com>
2022-03-02 07:30:15 -0600

    Add: Add FormData to sendIdea service

--- src/services/sendIdea.js
+++ src/services/sendIdea.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import { BACKEND_URI } from '../config'
 
-export default async function sendIdea({ id, content }) {
-  await axios.post(`${BACKEND_URI}/post/${id}`, content)
+export default async function sendIdea({ id, content, file }) {
+  const fd = new FormData()
+  fd.append('content', content)
+  if (file) {
+    fd.append('postImage', file)
+  }
+  await axios.post(`${BACKEND_URI}/post/${id}`, fd)
 }
