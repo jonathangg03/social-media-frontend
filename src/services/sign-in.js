6fe0cb3b6d28e5bc20c257464e5ce8c2cbd8089b
Jonathan García <megabyte9703@gmail.com>
2022-04-06 10:18:45 -0600

    Try other bar

--- src/services/sign-in.js
+++ src/services/sign-in.js
@@ -2,13 +2,10 @@ import axios from 'axios'
 // import { BACKEND_URI } from '../config'
 
 export default async function signIn({ email, password }) {
-  const response = await axios.post(
-    `${process.env.REACT_APP_API_URL}/auth/sign-in`,
-    {
-      email,
-      password
-    }
-  )
+  const response = await axios.post(`${process.env.API_URL}/auth/sign-in`, {
+    email,
+    password
+  })
 
   return response.data.body.jwt
 }
