f663cf71d0faf38b08ac876ab8be7b2afbf51065
Jonathan García <jona03g97@gmail.com>
2024-03-27 18:00:11 -0600

    Change: Temporaly delete api url env var

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL
+// const API_URL = process.env.REACT_APP_API_URL
+const API_URL = 'https://social-media-backend-qenx.onrender.com/auth/sign-in'
 
 export { API_URL }
